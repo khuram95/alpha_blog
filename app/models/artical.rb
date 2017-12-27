39189410559eb611fc4543fb49b729956845c8a9
khuram <khuram1995@gmail.com>
2017-12-27 13:59:32 +0000

    styling of index page

--- app/models/artical.rb
+++ app/models/artical.rb
@@ -1,4 +1,4 @@
 class Artical <ActiveRecord::Base
-    validates:title,presence:true,length:{minimum:3,maximum:20}
-    validates:description,presence:true,length:{minimum:5,maximum:200}
+    validates:title,presence:true,length:{minimum:3,maximum:100}
+    validates:description,presence:true,length:{minimum:5,maximum:2000}
 end

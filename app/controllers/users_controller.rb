11bb35fa0a9eaa7fd03b18eb971ba7ce947da864
khuram <khuram1995@gmail.com>
2017-12-29 16:10:27 +0000

    add create new account and edit this account of user routes and pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,26 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if(@user.save)
+            flash[:success] = "Your account was created successfully"
             redirect_to articals_path
         else
             render 'new'
         end
     end
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated successfully"
+             redirect_to articals_path
+        else
+             render 'edit'
+        end
+    end
+    
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)

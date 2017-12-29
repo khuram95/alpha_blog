c5095a5888a9ef1af5ea9684b0e6b98b9802bf45
khuram <khuram1995@gmail.com>
2017-12-29 11:09:43 +0000

    user migration and model file from user-creater branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220070221) do
+ActiveRecord::Schema.define(version: 20171229103414) do
 
   create_table "articals", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
